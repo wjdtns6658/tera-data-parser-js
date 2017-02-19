b3abf26814609f68ffb899d1f8645c7e1186c68f
meishu <theme97@gmail.com>
2017-02-19 02:39:13 -0800

    test: fill out testing for protocol#load()

--- test/protocol.js
+++ test/protocol.js
@@ -1,30 +1,83 @@
 const { test } = require('tap');
 const sinon = require('sinon');
 
+const path = require('path');
+
 // we have to be careful with this because the object stores state,
 // and we can't just re-require because node caches modules
 const protocol = require('../lib/protocol');
 
+// helper function
+function getTestDataPath(dir) {
+  return path.join(__dirname, dir, 'package.json');
+}
+
+const _console = {};
+
+for (const method of ['log', 'warn', 'error']) {
+  _console[method] = console[method].bind(console);
+}
+
+// tests
 test('load', (t) => {
-  const warn = sinon.spy(console, 'warn');
+  // set up
+  const warnings = [];
 
-  t.throws(() => protocol.load('invalid-directory'),
+  const warn = sinon.stub(console, 'warn', (...args) => {
+    if (args[0].startsWith('[protocol]')) {
+      warnings.push(args);
+    } else {
+      _console.warn(...args);
+    }
+  });
+
+  // test loads
+  t.throws(
+    () => protocol.load('invalid-directory'),
     /ENOENT: no such file or directory/,
-    'should throw if basePath points to invalid tera-data');
+    'should throw if basePath points to invalid tera-data'
+  );
 
-  t.ok(protocol.load(), 'should load default tera-data (from devDependencies)');
+  // devDependencies should have installed tera-data
+  t.ok(protocol.load(), 'should default load tera-data from node_modules');
 
-  // .map:
-  // TODO: malformed line
-  // TODO: non-numeric opcode
+  const ok = t.ok(
+    protocol.load(getTestDataPath('protocol-load')),
+    'should load tera-data from specified path'
+  );
 
-  // .def:
-  // TODO: malformed line
-  // TODO: array nesting too deep
+  if (!ok) t.bailout('cannot load custom tera-data paths for testing');
 
-  // TODO: unmapped message
+  // .map parsing
+  t.ok(
+    warnings.some(w => /parse error: non-numeric opcode\s+at ".+protocol\.map", line 5/.test(w)),
+    'should warn on non-numeric opcode in protocol.map'
+  );
 
-  console.warn.restore();
+  t.ok(
+    warnings.some(w => /parse error: malformed line\s+at ".+protocol\.map", line 6/.test(w)),
+    'should warn on malformed line in protocol.map'
+  );
+
+  // .def parsing
+  t.ok(
+    warnings.some(w => /parse error: malformed line\s+at ".+TEST_DEF\.def", line 4/.test(w)),
+    'should warn on malformed line in a .def'
+  );
+
+  t.ok(
+    warnings.some(w => /parse warning: array nesting too deep\s+at ".+TEST_DEF\.def", line 21/.test(w)),
+    'should warn on invalid array level in a .def'
+  );
+
+  // final checks
+  t.ok(
+    warnings.some(w => /unmapped message "UNMAPPED"/.test(w)),
+    'should warn on an unmapped message'
+  );
+
+  // tear down
+  warn.restore();
 
   t.end();
 });
