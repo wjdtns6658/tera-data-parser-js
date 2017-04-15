010e74f57173e0e2306f7de4dc797fc21d6a1357
meishu <theme97@gmail.com>
2017-04-15 14:42:59 -0700

    Fix tests for warnings

--- test/protocol.js
+++ test/protocol.js
@@ -4,6 +4,9 @@ const sinon = require('sinon');
 const path = require('path');
 const spec = require('./spec/protocol-spec.js');
 
+const util = require('util');
+const logger = require('../lib/logger');
+
 // we have to be careful with this because the object stores state,
 // and we can't just re-require because node caches modules
 const protocol = require('../lib/protocol');
@@ -13,28 +16,24 @@ function getTestDataPath(dir) {
   return path.join(__dirname, 'fixtures', dir);
 }
 
-const stubConsole = (() => {
-  const cons = {};
+function fakeLogger(array) {
+  return (...args) => {
+    const call = {};
 
-  for (const method of ['log', 'warn', 'error']) {
-    cons[method] = console[method].bind(console); // eslint-disable-line no-console
-  }
+    if (typeof args[0] === 'object') {
+      call.meta = args.shift();
+    }
 
-  return (method, array) =>
-    sinon.stub(console, method, (...args) => {
-      if (args[0].startsWith('[protocol]')) {
-        array.push(args);
-      } else {
-        cons.warn(...args);
-      }
-    });
-})();
+    call.message = util.format(...args);
+    array.push(call);
+  };
+}
 
 // tests
 test('load', (t) => {
   // set up
   const warnings = [];
-  const warn = stubConsole('warn', warnings);
+  const warn = sinon.stub(logger, 'warn').callsFake(fakeLogger(warnings));
 
   // test loads
   t.throws(
@@ -55,39 +54,39 @@ test('load', (t) => {
 
   // .map parsing
   t.ok(
-    warnings.some(w => /parse error: non-numeric opcode\s+at ".+protocol\.map", line 5/.test(w)),
+    warnings.some(w => /parse error: non-numeric opcode\s+at ".+protocol\.map", line 5/.test(w.message)),
     'should warn on non-numeric opcode in protocol.map'
   );
 
   t.ok(
-    warnings.some(w => /parse error: malformed line\s+at ".+protocol\.map", line 6/.test(w)),
+    warnings.some(w => /parse error: malformed line\s+at ".+protocol\.map", line 6/.test(w.message)),
     'should warn on malformed line in protocol.map'
   );
 
   // .def parsing
   t.ok(
-    warnings.some(w => /invalid filename syntax ".+NO_VERSION\.def"/.test(w)),
+    warnings.some(w => /invalid filename syntax ".+NO_VERSION\.def"/.test(w.message)),
     'should warn on invalid .def filename'
   );
 
   t.ok(
-    warnings.some(w => /parse error: malformed line\s+at ".+TEST_DEF\.1\.def", line 4/.test(w)),
+    warnings.some(w => /parse error: malformed line\s+at ".+TEST_DEF\.1\.def", line 4/.test(w.message)),
     'should warn on malformed line in a .def'
   );
 
   t.ok(
-    warnings.some(w => /parse warning: "count" or "offset" encountered, disabling implicit metatypes\s+at ".+TEST_DEF\.1\.def", line 17/.test(w)),
+    warnings.some(w => /parse warning: "count" or "offset" encountered, disabling implicit metatypes\s+at ".+TEST_DEF\.1\.def", line 17/.test(w.message)),
     'should warn on explicit "count"/"offset" type in a .def'
   );
 
   t.ok(
-    warnings.some(w => /parse warning: array nesting too deep\s+at ".+TEST_DEF\.1\.def", line 21/.test(w)),
+    warnings.some(w => /parse warning: array nesting too deep\s+at ".+TEST_DEF\.1\.def", line 21/.test(w.message)),
     'should warn on invalid array level in a .def'
   );
 
   // final checks
   t.ok(
-    warnings.some(w => /unmapped message "UNMAPPED"/.test(w)),
+    warnings.some(w => /unmapped message "UNMAPPED"/.test(w.message)),
     'should warn on an unmapped message'
   );
 
